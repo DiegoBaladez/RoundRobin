eea15996073d7bea78a324a82a34c27185994870
DiegoBaladez <87862229+DiegoBaladez@users.noreply.github.com>
2023-11-29 01:04:22 -0300

    Add files via upload
    Adicionando as trocas de filas de prioridade.

--- RoundRobin.c
+++ RoundRobin.c
@@ -1,65 +1,73 @@
-//teste para ver se commitou
-/*
-1) Escrever um algoritmo que simula a atuação do método de escalonamento Round Robin com feedback.
-
-2) Escrever um relatório: premissas + saídas do trabalho
-
-1.1) Como funciona o Round Robin?
-
-Premissas:
-
-*Limite máximo de processos criados: 4
-
-*Fatia de tempo dada para os processos: quanto tempo nosso processos ficaram com a CPU? Definir qtd de tempo.
-
-*Tempo de serviço e I/O: cada processo precisa ter seu tempo de serviço e uma quantidade aleatória de I/O
-
-*Tempo de I/O e suas possibilidades: Disco, Fita e impressora. 
-
-
-Escalonador com o Round Robin:
-
-
-Processos:
-cara processo precisa ter um PID. Cada processo precisa ter informação do PCB (Contexto de software: prioridade, pid,ppid,status)
-
-Escalona dor com 3 filas baseadas na sua prioridade: uma de alta e outra de baixa, 1 fila de I/O. Ordem de entrada: 
-    Processos novos: entram na fila de alta prioridade.
-
-
-
-
-
-
-O que precisamos fazer?
-1) Escrever uma struct de processos com as características aceima.
-
-2) Implementar uma função para o Round Robin.
-
-3) Criar essas 3 filas de prioridade.
-
-4) Criar uma função que escolhe qual processo aleatoriamente vai chegar primeiro no round robin.
-
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-int QUANTUM =  2;
-int qtdProcessos = 10;
+#include <time.h>
+
+int QUANTUM = 2;
+int qtdProcessos = 3;
 
 struct Processos {
     int tempoDeServico;
     int contadorDeTempoAtivo;
     int pid;
     int prioridade;
-    int status; // 0 = pronto, 1 =  suspenso,2 = bloqueado
-    int fazIO;
+    int status; // 0 = pronto, 1 = suspenso, 2 = bloqueado
+    int fazIO; //1 faz I/O e 0 nao faz I/O
     int tempoDeServicoDeIO;
     int contadorDeTempoAtivoEmIO;
-    int tipoDeIo; //0 - nao tem. 1 Fita, 2 Impressora, 3 Disco
+    int tipoDeIo; // 0 - não tem. 1 Fita, 2 Impressora, 3 Disco
 };
+
+// Protótipo de função para evitar conflito de declaração
+bool verificaSeEstaVazio(struct Processos *listaDeProcessos[], int tamanho);
+
+bool verificaSeEstaVazio(struct Processos *listaDeProcessos[], int tamanho) {
+    bool todasVazias = 1;
+
+    for (int i = 0; i < tamanho; i++) {
+        if (listaDeProcessos[i] != NULL) { //se for diferente de nulo, é pq tem algo lá.
+            todasVazias = 0; //todas vazias é falso.
+            break;
+        }
+    }
+
+    if (todasVazias) {
+        return true; //estao vazias
+    } else {
+        return false; //nao está vazio
+    }
+}
+
+void inserirNaPrimeiraPosicaoVazia(struct Processos *listaDeProcessos[], int tamanho, int elemento) {
+    bool posicaoVaziaEncontrada = false;
+
+    for (int i = 0; i < tamanho; i++) {
+        if (listaDeProcessos[i] == NULL) { 
+            listaDeProcessos[i] = elemento;
+            posicaoVaziaEncontrada = true;
+            break;
+        }
+    }
+
+    if (!posicaoVaziaEncontrada) {
+        printf("Não foi possível inserir o elemento. O array está cheio.\n");
+    }
+}
+
+void removerElemento(struct Processos *listaDeProcessos[], int tamanho, int posicao) {
+    if (posicao >= tamanho || posicao < 0) {
+        printf("Posição inválida!\n");
+        return;
+    }
+
+    // Move os elementos para a esquerda a partir da posição especificada
+    for (int i = posicao; i < tamanho - 1; i++) {
+        listaDeProcessos[i] = listaDeProcessos[i + 1];
+    }
+
+    // Define como vazio o último elemento do array após a movimentação
+    listaDeProcessos[tamanho - 1] = 0; // Aqui, 0 é considerado como "vazio"
+}
 
 struct Processos* criaProcesso() {
     struct Processos *novoProcesso = (struct Processos*)malloc(sizeof(struct Processos));
@@ -72,57 +80,115 @@ struct Processos* criaProcesso() {
     novoProcesso->tempoDeServico = rand() % (90 - 4 + 1) + 1;
     novoProcesso->contadorDeTempoAtivo = 0;
     novoProcesso->pid = rand() % (80 - 0 + 1) + 1;
-    novoProcesso->prioridade = rand() % (5 - 1 + 1) + 1;
-    novoProcesso->status = 0;
-    novoProcesso->fazIO = rand() % (90 - 4 + 1) + 1; //par para FazIO impar para NaoFazerIo
+    novoProcesso->prioridade = 0; 
+    novoProcesso->status = 0; 
+    novoProcesso->fazIO = rand() % 2;
     novoProcesso->contadorDeTempoAtivoEmIO = 0;
     novoProcesso->tipoDeIo = 0;
-    
-    if(novoProcesso->fazIO % 2 == 0)
-    {
+
+    if (novoProcesso->fazIO == 1) {
         novoProcesso->tempoDeServicoDeIO = rand() % (5 - 1 + 1) + 1;
-        novoProcesso->tipoDeIo = rand() % (4 - 0);
-    }
-    else{
+        novoProcesso->tipoDeIo = rand() % (3 - 0) + 1;
+    } else {
         novoProcesso->tempoDeServicoDeIO = 0;
-         novoProcesso->tipoDeIo = 0;
+        novoProcesso->tipoDeIo = 0;
     }
 
     return novoProcesso;
-};
+}
 
 bool roundRobin(struct Processos *listaDeProcessos[]) {
     struct Processos *processoAtual;
+    struct Processos *processoEmIo;
     int contaProcessosTerminados = 0;
+    int contaProcessosEmIo = 0;
+    int contaProcessosEmBaixaPrioridade = 0;
+    struct Processos *listaDeIO[qtdProcessos];
+    struct Processos *listaDeBaixaPrioridade[qtdProcessos];
+    
+    for (int i = 0; i < qtdProcessos; i++) {
+        listaDeIO[i] = NULL;
+    }
+
     int contaPrint = 0;
 
-    while(contaProcessosTerminados != qtdProcessos){
+    while (contaProcessosTerminados != qtdProcessos && contaProcessosEmIo == 0 && contaProcessosEmBaixaPrioridade == 0) {
         for (int i = 0; i < qtdProcessos; i++) {
-        
-        //processo escalonado pela CPU sendo retirado da fila de prontos
-        processoAtual = listaDeProcessos[i];
-        
-
-        if (processoAtual->contadorDeTempoAtivo >= processoAtual->tempoDeServico) continue;
-
-        // adiciona 1 unidade de tempo, até duas, no contador interno do processo
-        for (int j = 0; j < QUANTUM; j++) {
-            processoAtual->contadorDeTempoAtivo++;
-
-            // verifica se atingiu o tempo de serviço. Se sim, incrementa o contador de processos terminados.
-            if (processoAtual->contadorDeTempoAtivo == processoAtual->tempoDeServico) {
-                contaProcessosTerminados++;
-                printf("O processo com o PID %d, acabou com %d unidades de tempo:, sua posição na fila eh %d \n", processoAtual->pid,processoAtual->contadorDeTempoAtivo,i);
-                contaPrint ++;
-                break;
+            //por causa dessa MERDA de ponteiros, o array é criado já cheio de lixo dentro...
+            if (verificaSeEstaVazio(listaDeIO, qtdProcessos)) {
+                processoAtual = listaDeProcessos[i];
+            } else {
+                processoAtual = listaDeIO[0];
             }
-        }       
 
-        // realiza a circularidade do roundrobin para passar pelos programas que ainda não finalizaram
-        if (i == qtdProcessos - 1) i = -1;
+            //processoAtual = listaDeProcessos[i];
+            
+            if (processoAtual->tempoDeServicoDeIO != 0 && processoAtual->contadorDeTempoAtivoEmIO == 0)
+            {
+              processoAtual->status = 1;
+              removerElemento(listaDeProcessos,qtdProcessos,i);
+              contaProcessosEmIo++;
+              inserirNaPrimeiraPosicaoVazia(listaDeIO,qtdProcessos,processoAtual);                    
+            }
+
+            if(!verificaSeEstaVazio(listaDeIO,qtdProcessos))
+            {
+                printf("executando IO do processo\n");
+                processoEmIo = listaDeIO[0];
+                if(processoEmIo->contadorDeTempoAtivoEmIO == processoEmIo->tempoDeServicoDeIO)
+                {
+                    switch (processoEmIo->tipoDeIo)
+                    {
+                    case 1:
+                        printf("processo de IO do Processo %d terminou.\n", processoEmIo->pid);
+                        processoEmIo->fazIO = 0;
+                        removerElemento(listaDeIO,qtdProcessos,0);
+                        contaProcessosEmIo--;
+                        inserirNaPrimeiraPosicaoVazia(listaDeProcessos,qtdProcessos,processoEmIo);                                                
+                        break;
+                    
+                    case 2:
+                        printf("processo de IO do Processo %d terminou.\n", processoEmIo->pid);
+                        processoEmIo->fazIO = 0;
+                        removerElemento(listaDeIO,qtdProcessos,0);
+                        contaProcessosEmIo--;
+                        inserirNaPrimeiraPosicaoVazia(listaDeProcessos,qtdProcessos,processoEmIo);                                                
+                        break;
+
+                    case 3: //esse aqui precisa ir para uma fila  de baixa prioridade... mudar esse codigo
+                        printf("processo de IO do Processo %d terminou.\n", processoEmIo->pid);
+                        processoEmIo->fazIO = 0;
+                        removerElemento(listaDeIO,qtdProcessos,0);
+                        contaProcessosEmIo--;
+                        inserirNaPrimeiraPosicaoVazia(listaDeProcessos,qtdProcessos,processoEmIo);                                                
+                        break;
+
+                    default:
+                        break;
+                    }
+                }
+            }
+            //faltou o método de fazer I/O.. puts...
+            if (processoAtual->fazIO == 1) continue;
+
+            if (processoAtual->contadorDeTempoAtivo >= processoAtual->tempoDeServico) continue;
+
+            for (int j = 0; j < QUANTUM; j++) {
+                processoAtual->contadorDeTempoAtivo++;
+
+                if (processoAtual->contadorDeTempoAtivo == processoAtual->tempoDeServico) {
+                    contaProcessosTerminados++;
+                    printf("O processo com o PID %d, acabou com %d unidades de tempo:, sua posição na fila eh %d \n", processoAtual->pid, processoAtual->contadorDeTempoAtivo, i);
+                    contaPrint++;
+                    break;
+                }
+            }
+
+            if (i == qtdProcessos - 1) i = -1;
+        }
     }
-    }
-    printf("Contagem dos prints %d\n",contaPrint);
+
+    printf("Contagem dos prints %d\n", contaPrint);
     return true;
 }
 
@@ -131,18 +197,15 @@ int main(void) {
     srand(time(NULL));
     struct Processos *listaDeAltaPrioridade[qtdProcessos];
 
-    // preenche a lista de alta prioridade com os processos criados
     for (int i = 0; i < qtdProcessos; i++) {
         listaDeAltaPrioridade[i] = criaProcesso();
     }
 
-    // exibe os pids dos processos criados
-    for (int i = 0; i < qtdProcessos; i++) {        
-        printf("Esse processo foi criado com o PID = %d, está na posição da fila %d e possui o tempo de serviço igual a: %d \n", listaDeAltaPrioridade[i]->pid, i,listaDeAltaPrioridade[i]->tempoDeServico);
+    for (int i = 0; i < qtdProcessos; i++) {
+        printf("Esse processo foi criado com o PID = %d, está na posição da fila %d e possui o tempo de serviço igual a: %d \n", listaDeAltaPrioridade[i]->pid, i, listaDeAltaPrioridade[i]->tempoDeServico);
     }
-    
-    printf("\n\n");
 
+    printf("\n\n");
     terminou = roundRobin(listaDeAltaPrioridade);
 
     if (terminou) {
@@ -151,35 +214,9 @@ int main(void) {
         printf("Deu ruim\n");
     }
 
-    // libera a memória alocada para cada processo
     for (int i = 0; i < qtdProcessos; i++) {
         free(listaDeAltaPrioridade[i]);
     }
 
     return 0;
 }
-/*
-Atribui frações de tempo iguais para cada processo, manipula sem prioridade.
-Entra um processo, 
-
-Atributo Quantum. Processos armazenados em uma fila circular.
-
-
-    Recebe um array de processos
-    Tempo do processo ativo = TempoDeServiço - QUANTUM. 
-    Se tempo de processo ativo = tempo de serviço. pop da lista de 
-    As listas vão ficar dentro dele. Aí ele faz o controle das filas suspenso, bloqueado e pronto.
-    
-
-*/
-
-
-
-
-/*
-Recebo o processo.
-Aumento o seu tempo de atividade. Quando o incremento desse tempo for igual a = 2. 
-Preciso trocar para o proximo processo. 
-
-
-*/
